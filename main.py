072c523457db8a02d6d7f75f0fd9d8ee2332fe02
PrincessNosawema <167482755+PrincessNosawema@users.noreply.github.com>
2025-06-05 19:00:59 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from product import Product
+
+# Add a few sample products
+Product("Bag", "Women items", 5, "yellow and classy")
+Product("Watch", "Accessories", 25, "Leather strap, waterproof")
+
+# Interact with the product
+p = Product.all_products["bag"]
+p.view_info()
+p.display_summary()
+
+# Add more products via input
+Product.add_product()
